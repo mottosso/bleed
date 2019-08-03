af065ad53e980edcd1f5a5ff253595e9dcd3ab46
marcus <marcus@studioanima.co.jp>
2019-08-03 16:41:45 +0100

    Update README

---
+++ README.md
@@ -0,0 +1,7 @@
+### Bleed
+
+An Avalon example profile for [Allzpark](https://github.com/mottosso/allzpark).
+
+**Usage**
+
+- See https://allzpark.com/avalon
--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 late = locals()["late"]
 name = "bleed"
-version = "1.0.12"
+version = "1.0.14"
 build_command = "python -m rezutil build {root}"
 private_build_requires = ["rezutil-1"]
 
@@ -8,14 +8,13 @@ _requires = [
     "global",
 
     "~blender==2.80.0",
-    "~maya==2018.0.6",
+    "~maya==2015.0.0|2016.0.2|2017.0.4|2018.0.6",
 
     "python-3",
     "pyqt5-5.8",
     "pymongo-3.4+",
     "colorbleed-1",
     "avalon-5.2+",
-
 ]
 
 
@@ -38,14 +37,20 @@ def requires():
 
 
 def commands():
+    import os
     import tempfile
 
     global env
     global this
     global request
 
+    # Better suited for a global/studio package
+    projects = r"p:\projects" if os.name == "nt" else "~/projects"
+
+    env["AVALON_PROJECTS"] = projects
     env["AVALON_CONFIG"] = "colorbleed"
     env["AVALON_PROJECT"] = this.name
+    env["AVALON_EARLY_ADOPTER"] = "yes"
 
     if "maya" in request:
         env["PYTHONPATH"].append("{root}/maya")  # userSetup.py
